e64dd47a638e3490d76f6f35a178b216edfbf585
DThanh1508 <athanh2002kt@gmail.com>
2022-08-15 06:59:47 -0400

    Create list findByActive()

--- src/main/java/test/demo/repositories/MovieRepository.java
+++ src/main/java/test/demo/repositories/MovieRepository.java
@@ -1,7 +1,11 @@
 package test.demo.repositories;
+import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import test.demo.models.Movie;
 
 public interface MovieRepository extends MongoRepository<Movie, String> {
+//    List<Movie> findByTitle(String title);
+
+    List<Movie> findByActive(boolean b);
 }
