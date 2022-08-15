06cd97e295a9073d675f18b35b9b6f4bd0969d1a
DThanh1508 <athanh2002kt@gmail.com>
2022-08-15 06:58:11 -0400

    Add img & active

--- src/main/java/test/demo/models/Movie.java
+++ src/main/java/test/demo/models/Movie.java
@@ -2,6 +2,7 @@ package test.demo.models;
 
 import java.util.Date;
 
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,64 +10,15 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.List;
 
 @Document(collection = "movie")
+@Data
 @NoArgsConstructor
 public class Movie {
     @Id
     private String id;
     private String plot;
+    private String img;
     private List<String> genres;
     private String title;
     private Date creationDate = new Date();
-
-//    public Movie(String plot, List<String> genres, String title, Date creationDate) {
-//        this.plot = plot;
-//        this.genres = genres;
-//        this.title = title;
-//        this.creationDate = creationDate;
-//    }
-//    @Override
-//    public String toString(){
-//        return "Tutorial [Id: " + id + ", Plot: " + plot + ", Genres: " + genres + ", Title: " + title + ", Creation Date: "+ creationDate "]";
-//    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPlot() {
-        return plot;
-    }
-
-    public void setPlot(String plot) {
-        this.plot = plot;
-    }
-
-    public List<String> getGenres() {
-        return genres;
-    }
-
-    public void setGenres(List<String> genres) {
-        this.genres = genres;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public Date getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
-    }
-
+    private boolean active;
 }
